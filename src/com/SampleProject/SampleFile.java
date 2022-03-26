7cc4047b9913569060fb11db8510753ca36e544a
Prakash-27 <prakashs23007@gmail.com>
2022-03-26 03:56:16 -0500

    sample files for jenkins

---
+++ src/com/SampleProject/SampleFile.java
@@ -0,0 +1,13 @@
+package com.SampleProject;
+
+public class SampleFile {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Jenkins");
+		System.out.println("I'm Prakash S");
+		System.out.println("I'm Excited to Run my code with You.");
+
+	}
+
+}
