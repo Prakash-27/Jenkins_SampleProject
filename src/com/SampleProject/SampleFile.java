21d504896029ffd78ab83ad65e87db10791c0bad
Prakash-27 <73297233+Prakash-27@users.noreply.github.com>
2022-03-29 11:00:09 -0500

    saved changes

--- src/com/SampleProject/SampleFile.java
+++ src/com/SampleProject/SampleFile.java
@@ -3,11 +3,8 @@ package com.SampleProject;
 public class SampleFile {
 
 	public static void main(String[] args) {
-
 		System.out.println("Hello Jenkins");
 		System.out.println("I'm Prakash S");
 		System.out.println("I'm Excited to Run my code with You.");
-
 	}
-
 }
